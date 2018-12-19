6da80a9aefd92899973ba0b966eabf0b37a5662a
Yanko Nión Ferreiro <yanko.ferreiro@udc.es>
2018-12-19 20:32:51 +0100

    add test JUnitQuickCheck

--- src/test/java/net/gjerull/etherpad/client/EPLiteConnectionPropertiesTest.java
+++ src/test/java/net/gjerull/etherpad/client/EPLiteConnectionPropertiesTest.java
@@ -13,13 +13,27 @@ public class EPLiteConnectionPropertiesTest {
     private static final String API_VERSION = "1.2.12";
     private static final String ENCODING = "UTF-8";
 
-    @Property
+    @Property(trials = 15)
     public void domain_with_trailing_slash_when_construction_an_api_path(String exampleMethod) throws Exception {
 
         EPLiteConnection connection = new EPLiteConnection(
                 "http://example.com/", "apikey", API_VERSION, ENCODING
         );
         String apiMethodPath = connection.apiPath(exampleMethod);
+        
         assertEquals("/api/1.2.12/" + exampleMethod, apiMethodPath);
     }
+
+    @Property(trials = 15)
+	public void api_url_need_to_be_absolute(String apikey, String path) throws Exception {
+		try {
+			EPLiteConnection connection = new EPLiteConnection("http://example.com/", apikey, API_VERSION, ENCODING);
+			connection.apiUrl(path, null);
+			if (path.length() > 0) {
+				fail("Expected '" + EPLiteException.class.getName() + "' to be thrown");
+			}
+		} catch (EPLiteException e) {
+			assertTrue(true);
+		}
+	}
 }
